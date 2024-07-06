b07ce9536e91362f12c2ca0ed979174dbc311f78
Nick Thomas <nichoth@nichoth.com>
2024-07-05 21:50:51 -0700

    demo works

--- example/index.ts
+++ example/index.ts
@@ -1,6 +1,16 @@
-import '../src/index.css'
-import '../src/index.js'
+import '../src/eye-regular.js'
+import '../src/eye-slash.js'
+import './index.css'
 
 document.body.innerHTML += `
-    <example-component></example-component>
+    <div class="icons-demo">
+        <div>
+            <eye-regular></eye-regular>
+            regular eye
+        </div>
+        <div>
+            <eye-slash></eye-slash>
+            slash eye
+        </div>
+    </div>
 `
