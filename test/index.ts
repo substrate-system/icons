356fc397a849119f83bcb035b723615dcaf6d416
Nick Thomas <nichoth@nichoth.com>
2024-07-05 22:01:35 -0700

    fix the test

--- test/index.ts
+++ test/index.ts
@@ -1,14 +1,23 @@
 import { test } from '@bicycle-codes/tapzero'
 import { waitFor } from '@bicycle-codes/dom'
-import '../src/index.js'
+import '../src/eye-regular.js'
+import '../src/eye-slash.js'
 
 test('example', async t => {
-    document.body.innerHTML += `
-        <example-component class="test">
-        </example-component>
-    `
+    document.body.innerHTML += `<div class="icons-demo">
+        <div>
+            <eye-regular></eye-regular>
+            regular eye
+        </div>
+        <div>
+            <eye-slash></eye-slash>
+            slash eye
+        </div>
+    </div>`
 
-    const el = await waitFor('example-component')
+    const el = await waitFor('eye-regular')
+    const slash = await waitFor('eye-slash')
 
-    t.ok(el, 'should find an element')
+    t.ok(el, 'should find regular eye')
+    t.ok(slash, 'should find slash eye')
 })
